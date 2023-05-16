ab7c4a66fb0a673a65471690982b5abcb314a815
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-05-16 17:26:10 +0530

    created footer, added procfile

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express=require("express");
-const ejs=require("ejs");
-const bodyParser=require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 
-const app=express();
+const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index');
 });
 
-app.listen(3000,()=>{console.log("Server is running on port 3000");});
+app.listen(process.env.PORT || 3000, () => { console.log("Server is running on port 3000"); });
